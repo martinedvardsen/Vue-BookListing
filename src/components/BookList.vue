5b965803e1482e64aa44233e8fea1acd6e24415e
Martin Edvardsen <Martin.Edvardsen@isobar.com>
2020-11-19 11:17:50 +0100

    book from form pushed to list of books

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -3,18 +3,23 @@
   <h1>{{ title }}</h1>
   <ul>
     <book-item v-for="book in books" :book="book"></book-item>
+    <book-form @addBook="appendBook"></book-form>
   </ul>
 </div>
 </template>
 
 <script>
 import BookItem from './BookItem';
+import BookForm from './BookForm';
 
 export default {
   name: "BookList",
+
   components: {
-    BookItem
+    BookItem,
+    BookForm
   },
+
   data() {
     return {
       title: "All Books",
@@ -23,6 +28,15 @@ export default {
         {title: 'American Gods', author: 'Neil Gaiman'},
         {title: 'Amusing Ourselves to Death', author: 'Neil Postman'},
       ]
+    }
+  },
+
+  methods: {
+    appendBook(bookTitle, bookAuthor) {
+      this.books.push({
+        title: bookTitle,
+        author: bookAuthor
+      })
     }
   }
 }
